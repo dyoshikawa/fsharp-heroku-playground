d49d2fa125437adb1097b8df52e65e18f3ebdbe8
dyoshikawa <dyoshikawa1993@gmail.com>
2019-10-16 12:44:09 +0900

    fix

--- fsharp-heroku-playground/Program.fs
+++ fsharp-heroku-playground/Program.fs
@@ -1,11 +1,17 @@
 open System
 open Suave
 open Suave.Sockets
+open YoLo
 
 [<EntryPoint>]
-let main argv =
+let main _argv =
+    let portByEnv =
+        match Env.var ("PORT") with
+        | Some(x) -> x
+        | None -> "8080"
+
     let ip : Net.IPAddress = Net.IPAddress.Parse("0.0.0.0")
-    let port : Port = uint16 8080
+    let port : Port = uint16 portByEnv
 
     let socketBinding : Sockets.SocketBinding =
         { ip = ip
